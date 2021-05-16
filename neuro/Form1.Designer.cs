fdb501b4d1b39a02aaaa2a9585748936c3373188
Ruslan Sadikov <51824085+ruslans-s@users.noreply.github.com>
2021-05-16 13:27:17 +0300

    Добавьте файлы проекта.

---
+++ neuro/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace neuro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьВесыИзФайлаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.записатьВесыВФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(2, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(102, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(251, 420);
+            this.listBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(376, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(292, 219);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(2, 490);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(685, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(2, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.настройкиToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(699, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьВесыИзФайлаToolStripMenuItem,
+            this.записатьВесыВФайлToolStripMenuItem});
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            // 
+            // загрузитьВесыИзФайлаToolStripMenuItem
+            // 
+            this.загрузитьВесыИзФайлаToolStripMenuItem.Name = "загрузитьВесыИзФайлаToolStripMenuItem";
+            this.загрузитьВесыИзФайлаToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.загрузитьВесыИзФайлаToolStripMenuItem.Text = "Загрузить весы из файла";
+            this.загрузитьВесыИзФайлаToolStripMenuItem.Click += new System.EventHandler(this.загрузитьВесыИзФайлаToolStripMenuItem_Click);
+            // 
+            // записатьВесыВФайлToolStripMenuItem
+            // 
+            this.записатьВесыВФайлToolStripMenuItem.Name = "записатьВесыВФайлToolStripMenuItem";
+            this.записатьВесыВФайлToolStripMenuItem.Size = new System.Drawing.Size(211, 22);
+            this.записатьВесыВФайлToolStripMenuItem.Text = "Записать весы в файл";
+            this.записатьВесыВФайлToolStripMenuItem.Click += new System.EventHandler(this.записатьВесыВФайлToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 524);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьВесыИзФайлаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem записатьВесыВФайлToolStripMenuItem;
+    }
+}
+
---
+++ neuro/Form1.cs
@@ -0,0 +1,499 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace neuro
+{
+    struct TheardStruk
+    {
+        internal Vector fileVector;
+        internal Vector Y;
+    };
+
+    public partial class Form1 : Form
+    {
+
+         Network network = new Network(new int[] { 784, 16, 10 }); // создаём сеть с двумя входами, тремя нейронами в скрытом слое и одним выходом
+
+        public Form1()
+        {
+            InitializeComponent();
+            network.ReadFromFile();
+            DirectoryInfo dir = new DirectoryInfo(@"D:\repos\neuro\train\");
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                fileName.Add(file.Name);
+            }
+            listBox1.Items.Add(fileName.Count);
+            
+        }
+
+
+        List<string> fileName = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            progressBar1.Maximum = fileName.Count;
+
+            for (int l = 0; l < fileName.Count; l++)
+            {
+                network.ReadFromFile();
+                Bitmap newImage = new Bitmap(@"D:\repos\neuro\train\" + fileName[l]);
+
+                Vector fileVector = new Vector(784);
+                Vector Y = new Vector(10);
+
+                for (int j = 0; j < newImage.Width; j++)
+                    for (int i = 0; i < newImage.Height; i++)
+                    {
+                        //listBox1.Items.Add(Convert.ToInt32(newImage.GetPixel(i, i).R));
+                        fileVector[j * newImage.Width + i] = 0.00390625 * Convert.ToInt32(newImage.GetPixel(i, i).R);
+                    }
+                //Запись ответа упростить!!!!
+                string lineForRegex = Regex.Replace(fileName[l], ".*num", "");
+                lineForRegex = Regex.Replace(lineForRegex, ".png", "");
+                Y[Convert.ToInt32(lineForRegex)] = 1;
+
+                
+                //Пробуем в потоки
+                int theardCount = 15;
+                List<Thread> threads = new List<Thread>();
+                TheardStruk threadsStruct = new TheardStruk();
+                threadsStruct.fileVector = fileVector;
+                threadsStruct.Y = Y;
+
+                for (int i = 0; i < theardCount; i++)
+                {
+                    threads.Add(new Thread(() =>
+                        {
+                            fornewthread(fileVector, Y, network);
+                        }
+                        ));
+                    threads[i].Start();
+                }
+
+                listBox1.Items.Add("file");
+                
+
+                network.Train(fileVector, Y, 1 , 1e-7, 666); // запускаем обучение сети 
+                progressBar1.Value = progressBar1.Value + 1;
+                if (l == 200) break;
+
+            }
+            listBox1.Items.Add("Обучение закончено, записываем данные в файл");
+            network.WriteToFile();
+        }
+
+        static object locker = new object();
+
+        void fornewthread(Vector fileVector, Vector Y, Network network)
+        {
+
+            /*  if (obj.GetType() != typeof(TheardStruk))
+                  return;
+              TheardStruk tS = (TheardStruk)obj;*/
+
+            network.Train(fileVector, Y, 1, 1e-7, 666); // запускаем обучение сети 
+
+        }
+
+        int jk = 0;
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Bitmap newImage2 = new Bitmap(@"D:\repos\neuro\train\" + fileName[jk]);
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Load(@"D:\repos\neuro\train\" + fileName[jk]);
+
+            Vector fileVector3 = new Vector(784);
+
+            for (int j = 0; j < newImage2.Width; j++)
+                for (int i = 0; i < newImage2.Height; i++)
+                {
+                    fileVector3[j * newImage2.Width + i] = 0.00390625 * Convert.ToInt32(newImage2.GetPixel(i, i).R);
+                }
+
+            lock (locker)
+            {
+                Vector fileVector4 = network.Forward(fileVector3);
+
+                listBox1.Items.Clear();
+
+                listBox1.Items.Add(Convert.ToString(fileName[jk]));
+                double sample = 0,
+                    samplePos = 0;
+                for (int j = 0; j < 10; j++)
+                {
+                    listBox1.Items.Add(j + ": " + fileVector4[j]);
+                    if (fileVector4[j] > sample)
+                    {
+                        sample = fileVector4[j];
+                        samplePos = j;
+                    }
+                }
+                listBox1.Items.Add("Это: " + samplePos + " ? " + sample);
+            }
+            jk++;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void загрузитьВесыИзФайлаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            network.ReadFromFile();
+            listBox1.Items.Add("Параметры записаны в файл");
+        }
+
+        private void записатьВесыВФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            network.WriteToFile();
+            listBox1.Items.Add("Параметры загружены из файла");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+    class Vector
+    {
+        public double[] v; // значения вектора
+        public int n; // длина вектора
+
+        // конструктор из длины
+        public Vector(int n)
+        {
+            this.n = n; // копируем длину
+            v = new double[n]; // создаём массив
+        }
+
+        // создание вектора из вещественных значений
+        public Vector(params double[] values)
+        {
+            n = values.Length;
+            v = new double[n];
+
+            for (int i = 0; i < n; i++)
+                v[i] = values[i];
+        }
+
+        // обращение по индексу
+        public double this[int i]
+        {
+            get { return v[i]; } // получение значение
+            set { v[i] = value; } // изменение значения
+        }
+    }
+
+
+    class Matrix
+    {
+        double[][] v; // значения матрицы
+        public int n, m; // количество строк и столбцов
+
+        // создание матрицы заданного размера и заполнение случайными числами из интервала (-0.5, 0.5)
+        public Matrix(int n, int m, Random random)
+        {
+            this.n = n;
+            this.m = m;
+
+            v = new double[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = new double[m];
+
+                for (int j = 0; j < m; j++)
+                    v[i][j] = random.NextDouble() - 0.5; // заполняем случайными числами
+            }
+        }
+
+        // обращение по индексу
+        public double this[int i, int j]
+        {
+            get { return v[i][j]; } // получение значения
+            set { v[i][j] = value; } // изменение значения
+        }
+    }
+
+    class Network
+    {
+        struct LayerT
+        {
+            public Vector x; // вход слоя
+            public Vector z; // активированный выход слоя
+            public Vector df; // производная функции активации слоя
+        }
+
+
+        Matrix[] weights; // матрицы весов слоя
+        LayerT[] L; // значения на каждом слое
+        Vector[] deltas; // дельты ошибки на каждом слое
+
+        int layersN; // число слоёв
+
+        public void WriteToFile()
+        {
+            StreamWriter sw = new StreamWriter("scales.txt");
+            //Запись слоев
+            sw.WriteLine(layersN);
+
+            //Запись весов
+            for (int j = 0; j < weights.Length; j++)
+            {
+                for (int l = 0; l < weights[j].n; l++)
+                {
+                    for (int m = 0; m < weights[j].m; m++)
+                    {
+                        sw.WriteLine(weights[j][l, m]);
+                    }
+                }
+            }
+
+            // запись массивов значений на каждом слое
+            for (int j = 0; j < L.Length; j++)
+            {
+                for (int i = 0; i < L[j].x.n; i++)
+                {
+                    sw.WriteLine(L[j].x[i]);
+                }
+                for (int i = 0; i < L[j].z.n; i++)
+                {
+                    sw.WriteLine(L[j].z[i]);
+                }
+                for (int i = 0; i < L[j].df.n; i++)
+                {
+                    sw.WriteLine(L[j].df[i]);
+                }
+            }
+            // запись дельт
+            for (int j = 0; j < deltas.Length; j++)
+            {
+                for (int i = 0; i < deltas[j].n; i++)
+                {
+                    sw.WriteLine(deltas[j][i]);
+                }
+            }
+
+            sw.Close();
+        }
+
+        public void ReadFromFile()
+        {
+            StreamReader sw = new StreamReader("scales.txt");
+            //Запись слоев
+            // sw.WriteLine(layersN);
+            layersN = Convert.ToInt32(sw.ReadLine());
+
+            //Запись весов
+            for (int j = 0; j < weights.Length; j++)
+            {
+                for (int l = 0; l < weights[j].n; l++)
+                {
+                    for (int m = 0; m < weights[j].m; m++)
+                    {
+                        //  sw.WriteLine(weights[j][l, m]);
+                        weights[j][l, m] = Convert.ToDouble(sw.ReadLine());
+                    }
+                }
+            }
+
+            // запись массивов значений на каждом слое
+            for (int j = 0; j < L.Length; j++)
+            {
+                for (int i = 0; i < L[j].x.n; i++)
+                {
+                    L[j].x[i] = Convert.ToDouble(sw.ReadLine());
+                }
+                for (int i = 0; i < L[j].z.n; i++)
+                {
+                    L[j].z[i] = Convert.ToDouble(sw.ReadLine());
+                }
+                for (int i = 0; i < L[j].df.n; i++)
+                {
+                    L[j].df[i] = Convert.ToDouble(sw.ReadLine());
+                }
+            }
+            // запись дельт
+            for (int j = 0; j < deltas.Length; j++)
+            {
+                for (int i = 0; i < deltas[j].n; i++)
+                {
+                    deltas[j][i] = Convert.ToDouble(sw.ReadLine());
+                    // sw.WriteLine(deltas[j][i]);
+                }
+            }
+
+            sw.Close();
+        }
+
+        public Network(int[] sizes)
+        {
+            Random random = new Random(DateTime.Now.Millisecond); // создаём генератор случайных чисел
+
+            layersN = sizes.Length - 1; // запоминаем число слоёв
+
+            weights = new Matrix[layersN]; // создаём массив матриц весовых коэффициентов
+            L = new LayerT[layersN]; // создаём массив значений на каждом слое
+            deltas = new Vector[layersN]; // создаём массив для дельт
+
+            for (int k = 1; k < sizes.Length; k++)
+            {
+                weights[k - 1] = new Matrix(sizes[k], sizes[k - 1], random); // создаём матрицу весовых коэффициентов
+
+                L[k - 1].x = new Vector(sizes[k - 1]); // создаём вектор для входа слоя
+                L[k - 1].z = new Vector(sizes[k]); // создаём вектор для выхода слоя
+                L[k - 1].df = new Vector(sizes[k]); // создаём вектор для производной слоя
+
+                deltas[k - 1] = new Vector(sizes[k]); // создаём вектор для дельт
+            }
+        }
+
+        // прямое распространение
+        public Vector Forward(Vector input)
+        {
+            for (int k = 0; k < layersN; k++)
+            {
+                if (k == 0)
+                {
+                    for (int i = 0; i < input.n; i++)
+                        L[k].x[i] = input[i];
+                }
+                else
+                {
+                    for (int i = 0; i < L[k - 1].z.n; i++)
+                        L[k].x[i] = L[k - 1].z[i];
+                }
+
+                for (int i = 0; i < weights[k].n; i++)
+                {
+                    double y = 0;
+
+                    for (int j = 0; j < weights[k].m; j++)
+                        y += weights[k][i, j] * L[k].x[j];
+
+                    // активация с помощью сигмоидальной функции
+                    L[k].z[i] = 1 / (1 + Math.Exp(-y));
+                    L[k].df[i] = L[k].z[i] * (1 - L[k].z[i]);
+
+                    // активация с помощью гиперболического тангенса
+                    // L[k].z[i] = Math.Tanh(y);
+                    // L[k].df[i] = 1 - L[k].z[i] * L[k].z[i];
+
+                    //активация с помощью ReLU
+                    //  L[k].z[i] = y > 0 ? y : 0;
+                    // L[k].df[i] = y > 0 ? 1 : 0;
+                }
+            }
+
+            return L[layersN - 1].z; // возвращаем результат
+        }
+
+        // обратное распространение
+        void Backward(Vector output, ref double error)
+        {
+            int last = layersN - 1;
+
+            error = 0; // обнуляем ошибку
+
+            for (int i = 0; i < output.n; i++)
+            {
+                double e = L[last].z[i] - output[i]; // находим разность значений векторов
+
+                deltas[last][i] = e * L[last].df[i]; // запоминаем дельту
+                error += e * e / 2; // прибавляем к ошибке половину квадрата значения
+            }
+
+            // вычисляем каждую предудущю дельту на основе текущей с помощью умножения на транспонированную матрицу
+            for (int k = last; k > 0; k--)
+            {
+                for (int i = 0; i < weights[k].m; i++)
+                {
+                    deltas[k - 1][i] = 0;
+
+                    for (int j = 0; j < weights[k].n; j++)
+                        deltas[k - 1][i] += weights[k][j, i] * deltas[k][j];
+
+                    deltas[k - 1][i] *= L[k - 1].df[i]; // умножаем получаемое значение на производную предыдущего слоя
+                }
+            }
+        }
+
+        // обновление весовых коэффициентов, alpha - скорость обучения
+        void UpdateWeights(double alpha)
+        {
+            for (int k = 0; k < layersN; k++)
+            {
+                for (int i = 0; i < weights[k].n; i++)
+                {
+                    for (int j = 0; j < weights[k].m; j++)
+                    {
+                        double temp = alpha * deltas[k][i] * L[k].x[j];
+                        weights[k][i, j] -= temp;
+                    }
+                }
+            }
+        }
+
+        public void Train(Vector X, Vector Y, double alpha, double eps, int epochs)
+        {
+            int epoch = 1; // номер эпохи
+
+            double error; // ошибка эпохи
+
+            do
+            {
+                error = 0; // обнуляем ошибку
+
+                // проходимся по всем элементам обучающего множества
+
+                Forward(X); // прямое распространение сигнала
+                Backward(Y, ref error); // обратное распространение ошибки
+                UpdateWeights(alpha); // обновление весовых коэффициентов
+
+                //  Console.WriteLine("epoch: {0}, error: {1}", epoch, error); // выводим в консоль номер эпохи и величину ошибку
+
+                epoch++; // увеличиваем номер эпохи
+            } while (epoch <= epochs && error > eps);
+        }
+        public void Train(Vector[] X, Vector[] Y, double alpha, double eps, int epochs)
+        {
+            int epoch = 1; // номер эпохи
+
+            double error; // ошибка эпохи
+
+            do
+            {
+                error = 0; // обнуляем ошибку
+
+                // проходимся по всем элементам обучающего множества
+                for (int i = 0; i < X.Length; i++)
+                {
+                    Forward(X[i]); // прямое распространение сигнала
+                    Backward(Y[i], ref error); // обратное распространение ошибки
+                    UpdateWeights(alpha); // обновление весовых коэффициентов
+                }
+
+                //  Console.WriteLine("epoch: {0}, error: {1}", epoch, error); // выводим в консоль номер эпохи и величину ошибку
+
+
+
+                epoch++; // увеличиваем номер эпохи
+            } while (epoch <= epochs && error > eps);
+        }
+
+    }
+}
+
+
